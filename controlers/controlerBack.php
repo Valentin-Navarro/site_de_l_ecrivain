6a22a1aee677e47ac62b8d725aeb88b891d2cc90
Valentin-Navarro <Valentin-Navarro@users.noreply.github.com>
2018-03-20 22:01:01 +0100

    Modifs et rajouts

---
+++ controlers/controlerBack.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once ('model/model.php');
+
+function addArticle($author, $title, $content)
+{
+	$affectedLines = postArticle($author, $title, $content);
+
+	if ($affectedLines === false)
+	{
+		die('Erreur d\'ajout de l\'article');
+	}
+	else 
+	{
+		header('location: index.php?action=listPosts');
+	}	
+
+}
+
+function traitementConnexion($mail,$mdp)
+{
+	$mdp = sha1($mdp);
+	$membre = connexion($mail,$mdp);
+	
+	if ($membre === false)
+	{
+		die ('Erreur , les identifiants sont incorrects');
+	}
+	else 
+	{
+		$_SESSION['pseudo']= $membre['pseudo'] ; 
+		header('location: index.php?action=listPosts');
+	}
+
+
+}
+
+function validInscription($pseudo, $mail, $mdp)
+{
+	$mdp= sha1($mdp);
+	$affectedLines = addMembres($pseudo, $mail, $mdp);
+
+
+	if ($affectedLines === false)
+	{
+		die('Erreur d\'ajout du membre');
+	}
+	else 
+	{
+		header('location: index.php?action=listPosts'); 
+	}	
+}
+
+function deconnexion()
+{
+	if (session_start());
+	{
+		session_destroy();
+		header('location: index.php?action=listPosts'); 
+	}
+}
+
+}
--- controlers/controlerPublic.php
+++ controlers/controlerPublic.php
@@ -1,6 +1,60 @@
-<?php
+<?php 
+
+require_once ('model/model.php');
 
 function page404()
 {
 	require ('views/erreur_404.php');
+}
+
+function listPosts()
+{
+	$posts = getPosts();
+	require ('view/frontend/listPostView.php');
+}
+
+function post()
+{
+	$post = getPost($_GET['id']);
+	$comments = getComments($_GET['id']);
+
+	require ('view/frontend/postView.php');
+}
+
+function connexionForm ()
+{
+	require ('view/frontend/connexion.php');
+
+function formInscription()
+{
+	require ('view/frontend/inscription.php');
+}
+
+function addComment ($postId, $author,$comment)
+{
+	$affectedLines = postComment ($postId, $author, $comment);
+
+	if ($affectedLines === false)
+	{
+		die ('Erreur d\' ajout du commentaire');
+	}
+	else
+	{
+		header ('location: index.php?action=post&id=' . $postId);
+	}	
+}
+
+
+function addArticle($author, $title, $content)
+{
+	$affectedLines = postArticle($author, $title, $content);
+
+	if ($affectedLines === false)
+	{
+		die('Erreur d\'ajout de l\'article');
+	}
+	else 
+	{
+		header('location: index.php?action=listPosts');
+	}	
 }
