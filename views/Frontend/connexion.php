d6a8d5a8cee9bcdc25ea9afe9b20bfac5666f9ca
Valentin-Navarro <Valentin-Navarro@users.noreply.github.com>
2018-03-22 14:58:10 +0100

    modification bootstraps style

--- views/Frontend/connexion.php
+++ views/Frontend/connexion.php
@@ -1,23 +1,20 @@
 <?php $title = 'Blog de l ecrivain '; ?>
 
 <?php ob_start(); ?>
-<html>
-	<head>
-		<title>Blog de l'écrivain</title>
-		<meta charset="utf-8">
-	</head>
-	<body>
-		<div align="center">
-			<h2>Connexion</h2>
-			<br /><br />
-			<form method="POST" action="index.php?action=traitementConnexion">
-				<label for ="mail">Votre mail :</label>
-				<input type ="email" name="mailconnect" placeholder="Mail"/><br />
-				<label for ="password">Mot de Passe :</label>
-				<input type ="password" name="mdpconnect" placeholder="Mot de Passe"/>
-				<input type ="submit" name="formconnexion" placeholder="Se connecter"/>
-			</form>
-	</body>
-</html>	
+
+	<h2>Connexion</h2>
+			
+		<form class="py-3" method="POST" action="index.php?action=traitementConnexion">
+			<div class="form-group">
+				<label for="mail">Votre mail</label>
+				<input type="email" class="form-control" id="mail" placeholder="email@example.com" name="mailconnect">
+			</div>
+			<div class="form-group">
+				<label for="password">Mot de Passe</label>
+				<input type="password" class="form-control" id="password" placeholder="Password" name="mdpconnect">
+			</div>
+			<button type="submit" class="btn btn-primary">Se connecter</button>
+		</form>
+	
 <?php $content = ob_get_clean(); ?>
 <?php require('template.php'); ?>
--- views/Frontend/template.php
+++ views/Frontend/template.php
@@ -7,9 +7,9 @@
 		body {
 	  padding-top: 5rem;
 	}
-	.starter-template {
+	.starter-template 
+  {
 	  padding: 3rem 1.5rem;
-	  text-align: center;
 	}
 	</style>
 </head>
@@ -23,20 +23,11 @@
 
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Se connecter/ S'inscrire</button>
-        </form>
+        <div class=" my-2 my-lg-0">
+          <a class="btn btn-success my-2 my-sm-0" href="index.php?action=connexion" >Connexion</a>
+          <a class="btn btn-success my-2 my-sm-0" href="index.php?action=inscription">Inscription</a>
+        </div>
       </div>
     </nav>
       
