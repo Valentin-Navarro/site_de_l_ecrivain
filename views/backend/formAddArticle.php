23f4f8f77d0bfe816789886f492a3e59f22e891b
Valentin-Navarro <Valentin-Navarro@users.noreply.github.com>
2018-04-24 12:42:59 +0200

    Bootstrap

--- views/backend/formAddArticle.php
+++ views/backend/formAddArticle.php
@@ -1,25 +1,20 @@
 <?php $title = 'Blog de l ecrivain '; ?>
 
 <?php ob_start(); ?>
-    
+    <div>
     <h1>Blog de l'écrivain !</h1>
 
-			<form method="post" action="index.php?action=addArticle">
-				<table>
-					<tr>
-						<td>
-							<p>
-							<label for ="title"> Titre </label> :<input type="titre" name="title" id="title" required /></br>
-							<label for ="content">Message </label>
-							<textarea class="tinymce" name="content" id="content" required></textarea>
-							</p>
-						</td>	
-					</tr>
-				</table>	
 
-				<input type="submit" class="btn btn-success my-2 my-sm-0"value="Envoyer"/>
+<form method="post" action="index.php?action=addArticle">    
+    <div class="jumbotron jumbotron-fluid mb-5">
+  	<div class="container">
+    <h1 class="display-4 text-center mb-5">Nouvel Article </h1>
+    <h3 class="display-9 text-left mb-3"> Titre:<input type="titre" name="title" id="title" required /></h3>
+    <p class="tinymce" name="content" id="content" required></p>
+    <input type="submit" class="btn btn-success my-2 my-sm-0"value="Envoyer"/>
+  </div>
+</div>
 
-			</form>	
 
 
 
