a2a22df3c8996f9a227c2a1664fa72f8efbaf58b
Valentin-Navarro <Valentin-Navarro@users.noreply.github.com>
2018-08-31 15:00:03 +0200

    Indentation du code 2

--- views/backend/editionArticle.php
+++ views/backend/editionArticle.php
@@ -3,20 +3,20 @@
 
 <?php ob_start(); ?>
 
-<p> Bienvenue <?php echo $_SESSION['pseudo']; ?> </p>
+<p> Bienvenue <?php echo $pseudo; ?> </p>
 
-<form method="post" action="index.php?action=majArticle&id=<?= $billet['id'] ?>">    
-	<input type="hidden" name="idB" value="<?= $billet['id']?>"/>
+<form method="post" action="index.php?action=majArticle&id=<?= $billet->id ?>">    
+	<input type="hidden" name="idB" value="<?= $billet->id?>"/>
     <div class="jumbotron jumbotron-fluid mb-5">
   	<div class="container">
     <h1 class="display-4 text-center mb-5">Edition  </h1>
-    <h3 class="display-9 text-left mb-3"> Titre:<input type="titre" name="title" id="title" value="<?= $billet['title']?> "></h3>
-    <textarea class="tinymce" name="content" id="content"><?= $billet['content'] ?></textarea>
+    <h3 class="display-9 text-left mb-3"> Titre:<input type="titre" name="title" id="title" value="<?= $billet->title ?>"></h3>
+    <textarea class="tinymce" name="content" id="content"><?= $billet->content ?></textarea>
     <input type="submit" class="btn btn-success my-2 my-sm-0"value="Envoyer"/>
   </div>
 </div>
 
 
-<?php $content = ob_get_clean(); ?>
+<?php $content = ob_get_clean();?>
 
 <?php require('views/frontend/template.php'); ?>
--- views/backend/formAddArticle.php
+++ views/backend/formAddArticle.php
@@ -1,22 +1,21 @@
 <?php $title = 'Blog de l ecrivain '; ?>
 
 <?php ob_start(); ?>
-    <div>
-    <h1>Blog de l'écrivain !</h1>
+<div>
+	<h1>Blog de l'écrivain !</h1>
 
 
-<form method="post" action="index.php?action=addArticle">    
-    <div class="jumbotron jumbotron-fluid mb-5">
-  	<div class="container">
-    <h1 class="display-4 text-center mb-5">Nouvel Article </h1>
-    <h3 class="display-9 text-left mb-3"> Titre:<input type="titre" name="title" id="title" required /></h3>
-    <p class="tinymce" name="content" id="content" required></p>
-    <input type="submit" class="btn btn-success my-2 my-sm-0"value="Envoyer"/>
-  </div>
+	<form method="post" action="index.php?action=addArticle">    
+		<div class="jumbotron jumbotron-fluid mb-5">
+			<div class="container">
+				<h1 class="display-4 text-center mb-5">Nouvel Article </h1>
+				<h3 class="display-9 text-left mb-3"> Titre:<input type="titre" name="title" id="title" required /></h3>
+				<p class="tinymce" name="content" id="content" required></p>
+				<input type="submit" class="btn btn-success my-2 my-sm-0"value="Envoyer"/>
+			</div>
+		</div>
+	</form>	
 </div>
-
-
-
 
 <?php $content = ob_get_clean(); ?>
 
